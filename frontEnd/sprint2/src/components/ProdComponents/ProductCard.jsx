f86dbd5ba761dc8e32f313b36bd72a9705601ce7
afran012 <afran.012@gmail.com>
2022-02-07 20:34:29 -0500

    add prodmain styled

--- frontEnd/sprint2/src/components/ProdComponents/ProductCard.jsx
+++ frontEnd/sprint2/src/components/ProdComponents/ProductCard.jsx
@@ -1,4 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ProductCardDiv = styled.section `
+    margin: 5%;
+
+    background-color: #FFFFFF;
+    border-radius: 20px;
+    //border-width: 1px;
+    // border-style: solid;
+    // border-color: black;
+    width: 90%;
+    height: 112px;
+    display: grid; 
+    grid-template-columns: 30% 30% 30%;
+    grid-template-rows: 100% ;
+    align-self: center;
+    justify-self: center;
+
+    img{
+        //width: 100%;
+        max-width: 100%;
+        max-height: 100%;
+        border-radius: 20px;
+    }
+
+`
 
 const ProductCard = ({
     id,
@@ -8,7 +34,15 @@ const ProductCard = ({
 }) => {
 
 
-  return <div>{id} {product} {imagen} {precio}</div>;
+  return (<ProductCardDiv key={id}>
+    <img src={imagen} alt="..."/>
+    <section>
+        <p>{product}</p>
+        <p>{precio}</p>
+    </section>
+    
+
+    </ProductCardDiv>);
 };
 
 export default ProductCard;
