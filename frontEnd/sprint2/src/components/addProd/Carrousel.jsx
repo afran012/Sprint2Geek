06dda121a8cf3f9a5eda68e43f7250d2a46b37a9
afran012 <afran.012@gmail.com>
2022-02-09 14:11:01 -0500

    add carrousel

--- frontEnd/sprint2/src/components/addProd/Carrousel.jsx
+++ frontEnd/sprint2/src/components/addProd/Carrousel.jsx
@@ -1,69 +1,24 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
+import CountCarruContext from '../../context/CountCarruContext';
 import ProductsContext from '../../context/ProductsContext';
 import '../../styles/css/Carrousel.css'
-import { CarrouselContainer } from '../../styles/styleds/CarrouselStyled';
+
+import CarrouselImg from './CarrouselImg';
 
 
 
 const Carrousel = ({index = 3}) => {
 
   
-    const [Count, setCount] = useState({value:index});
+    const { Count, setCount } = useContext(CountCarruContext);
+
     const {Productos/*, setProductos*/} = useContext(ProductsContext)
-
-   // console.log(Productos.guajolotas[0].imagen);
-
-    
-    const [ImgPositions, setImgPositions] = useState({img0:Count.value,img1:Count.value+1,img2:Count.value+2});
-
-    const handlePosition = async() =>{
-        
-        if (Count.value>0) {
-            await setImgPositions({img0:Count.value,img1:Count.value+1,img2:Count.value+2})            
-        }
-        console.log(Count.value, ImgPositions);
-
-    }
-
-    const handleLeft = async ()=>{
-        console.log(Count.value);
-        if (Count.value>=0) {            
-            await setCount({value:Count.value-1})
-            await setImgPositions({img0:Count.value,img1:Count.value+1,img2:Count.value+2}) 
-            await handlePosition()
-        }
-
-    }
-
-    const handleRight = async ()=>{
-        if (Count.value<7) {
-            await setCount({value:Count.value+1})
-            await setImgPositions({img0:Count.value,img1:Count.value+1,img2:Count.value+2}) 
-            console.log(Count.value)
-            await handlePosition()            
-        } else {
-            console.log("right");
-        }                 
-    }
-
-    useEffect(() => {
-        console.log(ImgPositions);
-        //handlePosition()
-      return () => {
-        console.log("dismountCarusel");
-        console.log(ImgPositions);
-      };
-    }, [ ImgPositions]);
-    
 
   return (
       <>
-        <>{ImgPositions.img0}</>
-        <CarrouselContainer className='CarrouselContainer'>
-        <img className='carrouselImg1' src='https://res.cloudinary.com/dbyw7mbt6/image/upload/v1642700044/guappjolotas/verde_dcimlm.png' alt='...' onClick={()=>{handleLeft()}} />
-        <img className='carrouselImg2' src='https://res.cloudinary.com/dbyw7mbt6/image/upload/v1642700044/guappjolotas/Mole_quk0tw.png' alt='...' />
-        <img className='carrouselImg3' src='https://res.cloudinary.com/dbyw7mbt6/image/upload/v1642700043/guappjolotas/guayaba_buucjt.png' alt='...' onClick={()=>{handleRight()}} />
-        </CarrouselContainer>
+        <>{Count.value }</>
+        <CarrouselImg></CarrouselImg>
+
       </>
 
   );
---
+++ frontEnd/sprint2/src/context/CountCarruContext.js
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+
+const CountCarruContext = (createContext(null))
+
+export default CountCarruContext;
+
+
+
+
